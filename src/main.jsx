2ed7b6a0e8970cea3edc3f50bd38391e2884771e
Thiago Koch <tkoch.dev@gmail.com>
2023-12-26 12:23:31 -0300

    feat: install sass

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Page from './Page.jsx'
-import './index.css'
+import Page from './containers/Home/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
