e4953e9be04b4b6166c7a075e31150cd30bf039d
Thiago Koch <tkoch.dev@gmail.com>
2023-12-28 13:35:16 -0300

    feat: create Ancor component and init NavBarr component layout

---
+++ src/components/Ancor/index.jsx
@@ -0,0 +1,20 @@
+import './style.scss';
+import PropTypes from 'prop-types';
+
+export function Ancor(props) {
+  const {title, href, ...rest} = props;
+
+  return(
+    <>
+      <a className="ancorStructure" href={href} {...rest}>
+        {title}
+      </a>
+    </>
+  )
+
+}
+
+Ancor.propTypes = {
+  title: PropTypes.string,
+  href: PropTypes.string,
+};
--- src/components/NavBarr/index.jsx
+++ src/components/NavBarr/index.jsx
@@ -1,5 +1,34 @@
+import { Ancor } from '../Ancor'
 import './style.scss'
+import logo from "../../assets/logo-tkoch-sem-fundo.png"
 
-export function NavBarr() {
+export function NavBarr(props) {
+  const {link1, link2, link3, link4, link5, link6} = props;
 
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
+  return(
+    <>
+      <div className='navBarr'>
+
+        <img className='navBarr__logo' src={logo} alt="logo" onClick={scrollToTop}/>
+        
+        <nav className='navBarr__nav'>
+          <Ancor title='Sobre' href={link2}/>
+          <Ancor title='Conhecimento' href={link3}/>
+          <Ancor title='Educação' href={link4}/>
+          <Ancor title='Projetos' href={link5}/>
+          <Ancor title='Contato' href={link6}/>
+        </nav>
+
+
+
+      </div>
+    </>
+  )
 }
