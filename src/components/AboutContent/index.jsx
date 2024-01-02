080d2fe713081c41db6a92d8f58acc616a04b7b7
Thiago Koch <tkoch.dev@gmail.com>
2024-01-02 17:35:07 -0300

    feat: create aboutContent layout

--- src/components/AboutContent/index.jsx
+++ src/components/AboutContent/index.jsx
@@ -1,5 +1,33 @@
-import './style.scss'
+import './style.scss';
+import aboutData from '../../utils/aboutData.json';
 
 export function AboutContent() {
 
+  const subjects = aboutData;
+
+  return (
+    <>
+      <div className='aboutContent'>
+
+        <div className='aboutContent__title'>
+          Sobre Mim
+        </div>
+
+        <div className='aboutContent__main'>
+          {subjects.map(subject => (
+            <div className='aboutContent__subjectBlock' key={(String(subject.title))}>
+              <h2 className='aboutContent__subjectTitle'>
+                {subject.title}
+              </h2>
+              <p className='aboutContent__subjectText'>
+                {subject.text}
+              </p>
+            </div>
+          ))}
+        </div>
+        
+      </div>
+    
+    </>
+  )
 }
