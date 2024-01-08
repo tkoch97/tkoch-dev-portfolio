980706c3549cf81fe5aa07b166326fb95d4d1325
Thiago Koch <tkoch.dev@gmail.com>
2024-01-08 14:12:38 -0300

    feat: create aboutContent component layout

--- src/components/EducationContent/index.jsx
+++ src/components/EducationContent/index.jsx
@@ -1,5 +1,40 @@
 import './style.scss'
+import educationData from '../../utils/educationData.json';
+import { SectionsTitle } from '../SectionsTitle';
 
 export function EducationContent() {
 
+  const informations = educationData;
+  console.log(informations);
+
+  return (
+    <>
+    <div className='educationContent'>
+      <SectionsTitle text='Educação'/>
+
+      <div className='educationContent__main'>
+        {informations.map(information => (
+          <div className='educationContent__informationBlock' key={String(information.index)}>
+            <h2 className='educationContent__course'>
+              {information.course}
+            </h2>
+            <p className='educationContent__level'>
+              {information.level}
+            </p>
+            <p className='educationContent__institution'>
+              {information.institution}
+            </p>
+            <p className='educationContent__period'>
+              {information.period}
+            </p>
+            <p className='educationContent__about'>
+              {information.about}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+    
+    </>
+  )
 }
