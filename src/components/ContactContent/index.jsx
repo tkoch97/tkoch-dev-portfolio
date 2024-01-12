7b759ed4fc592f4d2a72cf6a56b7a0c78b5d4fb4
Thiago Koch <tkoch.dev@gmail.com>
2024-01-12 12:27:41 -0300

    feat: init ContactSection layout and create ContactForm

--- src/components/ContactContent/index.jsx
+++ src/components/ContactContent/index.jsx
@@ -1,5 +1,28 @@
+import { ContactForm } from '../ContactForm'
 import './style.scss'
 
 export function ContactContent() {
 
+  const placeholders = [
+    {
+      name:"Seu nome",
+      email:"Seu email",
+      message:"Escreva sua mensagem aqui",
+    }
+  ]
+
+  return(
+    <>
+      <div className='contactContent'>
+
+        <h2 className='contactContent__title'>
+          Me envie um email através do formulário abaixo
+        </h2>
+
+        <ContactForm placeholders={placeholders}/>
+
+      </div>
+    
+    </>
+  )
 }
